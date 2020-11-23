5d38b3337cd5b6645164c756b8dd3bb4ad196cf8
Walter Setti <walter.setti@iit.it>
2020-11-23 12:00:15 +0100

    Image

--- Fibonacci_AudioVideo/Form1.Designer.cs
+++ Fibonacci_AudioVideo/Form1.Designer.cs
@@ -34,6 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Fibonacci_AudioVideo.Properties.Resources.galaxy;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Name = "Main";
             this.Text = "POSTAZIONE AUDIO-VISIVA";
