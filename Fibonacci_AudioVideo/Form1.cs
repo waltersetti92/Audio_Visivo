d9420217d8856259015aa2ae9b08aeb011aeec03
Walter Setti <walter.setti@iit.it>
2020-11-23 11:25:03 +0100

    Rep Creata

---
+++ Fibonacci_AudioVideo/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Postazione Audio_Visiva
+namespace Fibonacci_AudioVideo
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
